6438222d0aa3785de6e3b7e9313fd36ffd4a8ee3
Terry Threatt <terry.threatt@gmail.com>
2020-01-01 21:36:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   while counter < array.length do 
-    puts arr1 = array[counter] ** 2 
+    puts arr1 = array[counter] ** 2
     counter += 1
   end
   return arr1
